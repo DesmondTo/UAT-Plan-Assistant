2cf51eb7bdba7786755ab53b054be7af5be72e5a
DesmondTo <e0550619@u.nus.edu>
2022-06-22 17:20:54 +0800

    Render form based on action selected

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import Header from "./Header";
-import HeroList from "./HeroList";
+import ActionDropdown from "./ActionDropdown";
 import Progress from "./Progress";
+import FormContainer from "./form/FormContainer";
 import InitializeProjectForm from "./form/InitializeProjectForm";
+import AddProjectActivityForm from "./form/AddProjectActivityForm";
+import AddActivityTypeForm from "./form/AddActivityType";
+import AddActivityForm from "./form/AddActivityForm";
+import AddStatusKeyForm from "./form/AddStatusKeyForm";
+import AddActionPartyForm from "./form/AddActionPartyForm";
+import EditProjectForm from "./form/EditProjectForm";
+import EditProjectActivityForm from "./form/EditProjectActivityForm";
+import EditActivityTypeForm from "./form/EditActivityTypeForm";
+import EditActivityForm from "./form/EditActivityForm";
+import EditStatusKeyForm from "./form/EditStatusKeyForm";
+import EditActionPartyForm from "./form/EditActionPartyForm";
+
+const actionComponent = {
+  addProj: <InitializeProjectForm />,
+  addProjAct: <AddProjectActivityForm />,
+  addActType: <AddActivityTypeForm />,
+  addAct: <AddActivityForm />,
+  addStat: <AddStatusKeyForm />,
+  addActPar: <AddActionPartyForm />,
+  editProj: <EditProjectForm />,
+  editProjAct: <EditProjectActivityForm />,
+  editActType: <EditActivityTypeForm />,
+  editAct: <EditActivityForm />,
+  editStat: <EditStatusKeyForm />,
+  editActPar: <EditActionPartyForm />,
+};
 
 /* global console, Excel, require */
 export default function App({ title, isOfficeInitialized }) {
+  const [action, setAction] = useState();
+
   if (!isOfficeInitialized) {
     return (
       <Progress
@@ -18,12 +46,12 @@ export default function App({ title, isOfficeInitialized }) {
     );
   }
 
+  // let Component = actionComponent[action];
+
   return (
     <div className="ms-welcome">
-      <Header logo={require("./../../../assets/logo-filled.png")} title={title} message="Hello" />
-      <HeroList message="Start your project!" items={[]}>
-        <InitializeProjectForm />
-      </HeroList>
+      <ActionDropdown selectAction={setAction} />
+      <FormContainer form={actionComponent[action]} />
     </div>
   );
 }
