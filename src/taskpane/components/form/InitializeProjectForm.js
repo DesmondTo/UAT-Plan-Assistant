76f6daec7e465adf9c19a642de6992547c6a6848
DesmondTo <e0550619@u.nus.edu>
2022-06-22 17:19:17 +0800

    Update form style

--- src/taskpane/components/form/InitializeProjectForm.js
+++ src/taskpane/components/form/InitializeProjectForm.js
@@ -4,28 +4,32 @@ import { DefaultButton } from "@fluentui/react";
 import { TextField } from "@fluentui/react";
 
 import { initializeProject } from "../../utils/projectUtils/projectInitializer";
+import FormActionContainer from "./FormActionContainer";
+/* global document, Office, module, require */
 
 function InitializeProjectForm() {
   const [projectName, setProjectName] = React.useState("");
   const [kickOffDate, setKickOffDate] = React.useState();
 
   return (
-    <form>
-      <TextField
-        label="Enter your project name:"
-        value={projectName}
-        onChange={(e) => setProjectName(e.target.value)}
-      />
-      <TextField
-        type="date"
-        label="Enter your project kick-off date:"
-        value={kickOffDate}
-        onChange={(e) => setKickOffDate(e.target.value)}
-      />
-      <DefaultButton className="btn-danger" onClick={() => initializeProject(projectName, kickOffDate)}>
-        Initialize Project
-      </DefaultButton>
-    </form>
+    <>
+      <form>
+        <TextField
+          label="Enter your project name:"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+        />
+        <TextField
+          type="date"
+          label="Enter your project kick-off date:"
+          value={kickOffDate}
+          onChange={(e) => setKickOffDate(e.target.value)}
+        />
+        <FormActionContainer>
+          <DefaultButton onClick={() => initializeProject(projectName, kickOffDate)}>Initialize Project</DefaultButton>
+        </FormActionContainer>
+      </form>
+    </>
   );
 }
 
